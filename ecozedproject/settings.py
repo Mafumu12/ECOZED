38571d7534104d611c9dad5b671fd035f28f857c
Mafumu12 <mafumumumba12112@gmail.com>
2023-12-26 11:05:30 +0200

    db conection

--- ecozedproject/settings.py
+++ ecozedproject/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'ecozedproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ECOZED',
+        'USER': 'root',
+        'PASSWORD': 'MEANmag12112',
+        'HOST': 'localhost',
+        'PORT': '3306',  # Default MySQL port
     }
 }
 
